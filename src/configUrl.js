d020f90caceca7ac501c28f4a179668fdfd203c3
jineshshah09 <68469170+jineshshah09@users.noreply.github.com>
2023-01-01 23:19:27 +0530

    Backend url updated

--- src/configUrl.js
+++ src/configUrl.js
@@ -1,2 +1,2 @@
-export const REACT_API_ENDPOINT = "https://4q931ru18g.execute-api.ap-south-1.amazonaws.com/test";
+export const REACT_API_ENDPOINT = "https://fjm3dot6q2.execute-api.ap-south-1.amazonaws.com/prd";
 // export const REACT_API_ENDPOINT = "http://192.168.1.100:5000";
